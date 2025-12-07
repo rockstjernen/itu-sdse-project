bb42f3a6988b4d7f1443a62d7153101536bbd2ea
mikke <mikke@itu.dk>
2025-12-07 23:27:16 +0100

    dagger + github workflow test

---
+++ dagger/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"dagger/itu-sdse-project/internal/dagger"
+)
+
+type ItuSdseProject struct{}
+
+// Train builds the container and runs the training pipeline, returning the model file
+func (m *ItuSdseProject) Train(ctx context.Context, source *dagger.Directory) *dagger.File {
+	// Build container from source directory using our Dockerfile
+	container := dag.Container().
+		Build(source, dagger.ContainerBuildOpts{
+			Dockerfile: "train_dummy.dockerfile",
+		})
+
+	// Run the container (executes the CMD from Dockerfile)
+	trained := container.WithExec([]string{"python", "-m", "itu_sdse_project.pipeline_dummy"})
+
+	// Return the model file
+	return trained.File("/app/models/model.pkl")
+}
